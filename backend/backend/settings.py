3c600272cf5f59e581c53e8d33d83bfc77cb1a42
zekoff <zekoff@gmail.com>
2022-01-02 23:53:52 +0000

    Commit CSRF fixes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-cl1(@(0c1h(we4ka0o81xde8#!mycd_0ju509t_y!h%f3s^_-r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*',
+]
 
 
 # Application definition
@@ -37,9 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
+    'todo',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +127,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_REPLACE_HTTPS_REFERRER = True
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.gitpod.io',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'https://*.gitpod.io',
+]
