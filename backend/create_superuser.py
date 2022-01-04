c1c6bb24e1b972486dff3bc9618469dbafece1ad
zekoff <zekoff@gmail.com>
2022-01-04 15:03:58 +0000

    Add frontend

--- backend/create_superuser.py
+++
@@ -1,6 +0,0 @@
-from django.core import management
-import os
-
-print('Inside create_superuser.py')
-print(os.environ.get('DJANGO_SUPERUSER_USERNAME'))
-management.call_command('createsuperuser', interactive=False)
