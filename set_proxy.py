8af6f965c0b4e79ce7f2ff9a2bcbf1d084755bee
zekoff <zekoff@gmail.com>
2022-01-08 04:26:25 +0000

    Strip invalid newline char when setting proxy

--- set_proxy.py
+++ set_proxy.py
@@ -11,7 +11,7 @@ package_contents = None
 with open(PKG_FILENAME, 'r', encoding='utf-8') as package_file:
     package_contents = json.load(package_file)
 
-proxy = subprocess.run(['gp', 'url', '3000'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+proxy = subprocess.run(['gp', 'url', '3000'], stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
 print(proxy)
 package_contents['proxy'] = proxy
 
